0330023a210314a8f685cfb1a92cba7e177dbf6f
Chaz Jenkins <>
2019-12-24 13:57:19 -0500

    Working on strings
    -CJ

---
+++ CSharpFundamentals/Program.cs
@@ -0,0 +1,35 @@
+﻿using CSharpFundamentals.Math;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[3];
+            numbers [0] = 1;
+
+            System.Console.WriteLine(numbers[0]);
+            System.Console.WriteLine(numbers[1]);
+            System.Console.WriteLine(numbers[2]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+
+            System.Console.WriteLine(flags[0]);
+            System.Console.WriteLine(flags[1]);
+            System.Console.WriteLine(flags[2]);
+
+            var names = new string[3] { "Patrice O'Neal", "Colin Quinn", "Steve Dildarian" };
+
+            /*var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            System.Console.WriteLine(result);*/
+        }
+    }
+}
