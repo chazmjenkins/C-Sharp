93bcc501aecf84c82e2d178e38c0f3f0186bf6d6
Chaz Jenkins <>
2019-12-24 12:36:55 -0500

    Very basic C# code
    -CJ

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # C-Sharp
- Repo for C# Code by Chaz M. Jenkins
+
+Repo for C# Code by Chaz M. Jenkins
+
+This code will be of low complexity for time being. Will likely add increase complexity Q2 2020.
---
+++ TypeConversion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+
+                //var number = "12345";
+                //int z = Convert.ToByte(number);
+                //Console.WriteLine(z);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to a byte.");
+
+            }
+
+
+
+        }
+    }
+}
