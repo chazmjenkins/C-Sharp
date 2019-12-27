a342fa123b8c4f691e7d9edb6011743ebad33f96
Chaz Jenkins <>
2019-12-26 23:23:04 -0500

    Exercise 2
    5 problems to solve.

---
+++ RandomClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RandomClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int passwordLength = 10;
+            var Random = new Random();
+            char[] buffer = new char[passwordLength];
+            for (var i = 0; i < passwordLength; i++)
+                buffer[i] = ((char)('a' + Random.Next(0, 26)));
+            var password = new string(buffer);
+            Console.WriteLine(password);
+        }
+    }
+}
